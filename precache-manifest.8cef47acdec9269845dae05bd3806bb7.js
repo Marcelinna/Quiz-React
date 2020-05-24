0a172a874440e8799b38c8f024bbc00de75b2b15
Marcelina <marcelinasiwecka@gmail.pl>
2020-05-24 23:35:29 +0200

    Updates

---
+++ precache-manifest.8cef47acdec9269845dae05bd3806bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5468a77b806b2b02955ecc05a85d0681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0b995cfee0a61ef4ba3",
+    "url": "/static/css/main.322d85fc.chunk.css"
+  },
+  {
+    "revision": "e817fd0720456ccaf563",
+    "url": "/static/js/2.eaa54616.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eaa54616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b995cfee0a61ef4ba3",
+    "url": "/static/js/main.1d0fb719.chunk.js"
+  },
+  {
+    "revision": "c50f07be9abc56c04c94",
+    "url": "/static/js/runtime-main.7ea3045c.js"
+  }
+]);
