ce814e937d5b8279157234dff8a5bf98ecc80f42
Marcelina <marcelinasiwecka@gmail.pl>
2020-05-24 23:58:40 +0200

    Updates

---
+++ precache-manifest.6c747238540c00b12d4ba0effb609422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc92195e8b837fda6920a02a55d35701",
+    "url": "/Quiz-React/index.html"
+  },
+  {
+    "revision": "91e32935421cfb9f4c1e",
+    "url": "/Quiz-React/static/css/main.322d85fc.chunk.css"
+  },
+  {
+    "revision": "e817fd0720456ccaf563",
+    "url": "/Quiz-React/static/js/2.eaa54616.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-React/static/js/2.eaa54616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e32935421cfb9f4c1e",
+    "url": "/Quiz-React/static/js/main.dd1c8776.chunk.js"
+  },
+  {
+    "revision": "5183dbe850972c5a180d",
+    "url": "/Quiz-React/static/js/runtime-main.03f84687.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cef47acdec9269845dae05bd3806bb7.js"
+  "/Quiz-React/precache-manifest.6c747238540c00b12d4ba0effb609422.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Quiz-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
